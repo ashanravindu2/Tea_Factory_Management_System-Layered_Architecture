279218c94e647a3678652352a5d7a14a77dccf7b
ashanravindu2 <ravindulakshithaashan@gmail.com>
2024-01-10 01:29:06 +0530

    type  already

--- src/main/java/lk/captain/model/FuelManageModel.java
+++ src/main/java/lk/captain/model/FuelManageModel.java
@@ -41,30 +41,6 @@ public class FuelManageModel {
 
 
 
-    public List<FuelMaterialDTO> getAllFuel() throws SQLException {
-        Connection connection = DbConnection.getInstance().getConnection();
-
-        String sql = "SELECT matirials.barrelId, bCategory, bLeter\n" +
-                "FROM matirials\n" +
-                "WHERE  bCategory IS NOT NULL\n" +
-                "  AND bLeter IS NOT NULL;";
-        PreparedStatement pstm = connection.prepareStatement(sql);
-
-        List<FuelMaterialDTO> dtoList = new ArrayList<>();
-
-        ResultSet resultSet = pstm.executeQuery();
-
-        while (resultSet.next()) {
-            String barrelId = resultSet.getString(1);
-            String bCategory = resultSet.getString(2);
-            double bLeter = resultSet.getDouble(3);
-
-
-            var dto = new FuelMaterialDTO(barrelId, bCategory, bLeter);
-            dtoList.add(dto);
-        }
-        return dtoList;
-    }
 
 
 
@@ -94,7 +70,18 @@ public class FuelManageModel {
         }
         return dtoList;
     }
+    public boolean usedUpdateFuel(String id ,double liter) throws SQLException {
+        Connection connection = DbConnection.getInstance().getConnection();
 
+        String sql = "UPDATE matirials SET bLeter = bLeter-? WHERE barrelId= ?";
+        PreparedStatement pstm = connection.prepareStatement(sql);
+
+        pstm.setDouble(1,liter );
+        pstm.setString(2,id );
+
+
+        return pstm.executeUpdate() > 0;
+    }
 
         public ResultSet getAllAvalable() throws SQLException {
             Connection connection = DbConnection.getInstance().getConnection();
