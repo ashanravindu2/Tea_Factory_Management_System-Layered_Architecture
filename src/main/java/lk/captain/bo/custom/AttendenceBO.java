d96edea10769118914f40308c21a8b2b43456f50
ashanravindu2 <ravindulakshithaashan@gmail.com>
2024-01-11 02:52:37 +0530

    DAO already

---
+++ src/main/java/lk/captain/bo/custom/AttendenceBO.java
@@ -0,0 +1,27 @@
+package lk.captain.bo.custom;
+
+import lk.captain.bo.SuperBO;
+import lk.captain.dao.SQLUtil;
+import lk.captain.db.DbConnection;
+import lk.captain.dto.AttendenceDTO;
+import lk.captain.entity.Attendence;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
+public interface AttendenceBO extends SuperBO {
+
+    public boolean manage(AttendenceDTO attendenceDTO) throws SQLException;
+
+    public ArrayList<AttendenceDTO> getAllAttendeceDetail() throws SQLException, ClassNotFoundException;
+
+    public boolean update(AttendenceDTO dto) throws SQLException, ClassNotFoundException;
+
+    public int issearchAttendenceCount(String id,String isdate1 ,String isdate2) throws SQLException, ClassNotFoundException;
+
+    public AttendenceDTO searchEmplIsDate(String id, String date) throws SQLException, ClassNotFoundException;
+
+    public ResultSet isTodayAtt() throws SQLException, ClassNotFoundException;
+}
---
+++ src/main/java/lk/captain/bo/custom/AttendenceBOImpl.java
@@ -0,0 +1,72 @@
+package lk.captain.bo.custom;
+
+import lk.captain.dao.DAOFactory;
+import lk.captain.dao.custom.AddCustomerDAO;
+import lk.captain.dao.custom.AttendenceDAO;
+import lk.captain.dto.AttendenceDTO;
+import lk.captain.entity.AddCustomer;
+import lk.captain.entity.Attendence;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AttendenceBOImpl implements AttendenceBO{
+
+    AttendenceDAO attendenceDAO = (AttendenceDAO) DAOFactory.getDaoFactory().getDAO(DAOFactory.DAOTypes.ATTENDENCE);
+    @Override
+    public boolean manage(AttendenceDTO attendenceDTO) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public ArrayList<AttendenceDTO> getAllAttendeceDetail() throws SQLException, ClassNotFoundException {
+        ArrayList<Attendence> attendences = attendenceDAO.getAll();
+         ArrayList<AttendenceDTO> attendenceDTOS = new ArrayList<>();
+
+for (Attendence attendence : attendences) {
+             attendenceDTOS.add(new AttendenceDTO(
+                     attendence.getEmpAttenId(),
+                     attendence.getName(),
+                     attendence.getAttMark(),
+                     attendence.getDate(),
+                     attendence.getTime()
+             ));
+         }
+            return attendenceDTOS;
+    }
+
+    @Override
+    public boolean update(AttendenceDTO dto) throws SQLException, ClassNotFoundException {
+        return attendenceDAO.update(new Attendence(dto
+                .getEmpAttenId(),dto.getName(),dto.getAttMark(),dto.getDate(),dto.getTime()));
+    }
+
+
+    @Override
+    public int issearchAttendenceCount(String id, String isdate1, String isdate2) throws SQLException, ClassNotFoundException {
+        return attendenceDAO.issearchAttendenceCount(id,isdate1,isdate2);
+    }
+
+    @Override
+    public AttendenceDTO searchEmplIsDate(String id, String date) throws SQLException, ClassNotFoundException {
+        Attendence attendence = attendenceDAO.searchEmplIsDate(id,date);
+        AttendenceDTO attendenceDTO = new AttendenceDTO(
+                attendence.getEmpAttenId(),
+                attendence.getName(),
+                attendence.getAttMark(),
+                attendence.getDate(),
+                attendence.getTime()
+        );
+        return attendenceDTO;
+
+    }
+
+    @Override
+    public ResultSet isTodayAtt() throws SQLException, ClassNotFoundException {
+        return attendenceDAO.isTodayAtt();
+    }
+}
+
+
